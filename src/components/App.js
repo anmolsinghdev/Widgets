c2f7734a9d85bf5ff47fa7cd546cdfc646d30438
OHBOY <ohboy.web@gmail.com>
2021-11-01 22:33:08 +0530

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import Accordion from './Accordion';
+// import Accordion from './Accordion';
+import Search from './Search';
 
-const items = [
-  {
-    title: 'what is React?',
-    content: 'React is FrameWork',
-  },
-  {
-    title: 'what use of React?',
-    content: 'It is Basically used for the Devlopment of the website',
-  },
-  {
-    title: 'why we use Recat?',
-    content: 'React is useful a building a Web Apps',
-  },
-];
+// const items = [
+//   {
+//     title: 'what is React?',
+//     content: 'React is FrameWork',
+//   },
+//   {
+//     title: 'what use of React?',
+//     content: 'It is Basically used for the Devlopment of the website',
+//   },
+//   {
+//     title: 'why we use Recat?',
+//     content: 'React is useful a building a Web Apps',
+//   },
+// ];
 
 const App = () => {
   return (
     <div>
-      <Accordion items={items} />
+      {/* <Accordion items={items} /> */}
+      <Search />
     </div>
   );
 };
